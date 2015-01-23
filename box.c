20cb6ec7dc9c91af8e426005fcb9c793733d4027
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2015-01-23 23:32:20 +0100

    forgot to remove debugging always-bananor

--- box.c
+++ box.c
@@ -84,8 +84,7 @@ PowerUp boxes_check_hit(SDL_Rect car)
             boxes[i].hit_time = SDL_GetTicks();
             int random_number = rand() % (POWERUP_STAR - 1);
             random_number++; // the first is POWERUP_NONE
-            return POWERUP_BANANA;
-//            return (PowerUp)random_number;
+            return (PowerUp)random_number;
         }
     }
     return POWERUP_NONE;
