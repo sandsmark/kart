a96c5abc1f047c3d1ab1b650ab47b93d09c78622
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2015-04-02 15:14:46 +0200

    fix car size

--- car.c
+++ car.c
@@ -51,8 +51,6 @@ Car *car_add()
 	sprintf(filename, "car%d.bmp", i);
 	cars[i].texture = ren_load_image_with_dims(filename, &cars[i].width, &cars[i].height);
 
-	cars[i].width /= 1.5;
-	cars[i].height /= 1.5;
 	cars[i].name[0] = '0' + i;
 	cars[i].name[1] = 0;
 
